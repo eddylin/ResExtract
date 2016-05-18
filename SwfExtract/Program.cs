6f348b5c02eb511cd8309d7416731596d706cc7c
eddylin <lin-shirui@hotmail.com>
2016-05-18 15:04:50 +0800

    upload project

---
+++ SwfExtract/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SwfDotNet;
+using SwfDotNet.IO;
+
+namespace SwfExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileInfo file = new FileInfo(@"");
+
+            using (FileStream fs = file.OpenRead())
+            {
+                SwfReader reader = new SwfReader(fs);
+
+                Swf swf = reader.ReadSwf();
+            }
+        }
+    }
+}
